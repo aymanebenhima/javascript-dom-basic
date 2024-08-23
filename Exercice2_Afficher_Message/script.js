7be7569455faddd1792143967d19dd8d6dce01b5
AYMANE BENHIMA <aymanebenhima@gmail.com>
2024-08-23 18:46:51 +0100

    Create script.js

---
+++ Exercice2_Afficher_Message/script.js
@@ -0,0 +1,5 @@
+// Ajouter un écouteur d'événement au bouton pour détecter le clic
+
+// Récupérer la valeur du champ de texte (le nom entré par l'utilisateur)
+
+// Afficher un message de bienvenue personnalisé en utilisant la valeur récupérée
