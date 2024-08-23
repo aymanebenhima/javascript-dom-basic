c76d2f9675d8dadfb7fe2370de86c9a8e3988c6f
AYMANE BENHIMA <aymanebenhima@gmail.com>
2024-08-23 18:46:03 +0100

    Create script.js

---
+++ Exercice1_Changer_Couleur_Fond/script.js
@@ -0,0 +1,5 @@
+// Ajouter un écouteur d'événement au bouton pour détecter le clic
+
+// Créer une fonction pour changer la couleur de fond du body
+
+// (Optionnel) Créer une fonction auxiliaire pour générer une couleur aléatoire
