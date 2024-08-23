76eb63a023e72f7d9d9948d29158a25d451ada57
AYMANE BENHIMA <aymanebenhima@gmail.com>
2024-08-23 18:49:58 +0100

    Create script.js

---
+++ Exercice3_Compteur_Clics/script.js
@@ -0,0 +1,7 @@
+// Initialiser un compteur à zéro
+
+// Ajouter un écouteur d'événement au bouton pour détecter le clic
+
+// Incrémenter le compteur à chaque clic
+
+// Mettre à jour l'affichage du nombre de clics
