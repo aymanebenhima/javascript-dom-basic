2d96f0f750dd557dde54a3f9f59970844c062069
AYMANE BENHIMA <aymanebenhima@gmail.com>
2024-08-26 19:40:20 +0100

    Create script.js

---
+++ Exercice7_Changer_Image_Galerie/script.js
@@ -0,0 +1,5 @@
+// Sélectionner les éléments nécessaires
+
+// Ajouter un événement à chaque miniature pour changer l'image principale
+
+// Vous pouvez utiliser la propriété `src` pour changer l'image
