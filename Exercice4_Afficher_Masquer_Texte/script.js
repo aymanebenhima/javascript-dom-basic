79e4905810a2c3683dc8c9c08d6c8625fab3f9c3
AYMANE BENHIMA <aymanebenhima@gmail.com>
2024-08-26 19:24:33 +0100

    Create script.js

---
+++ Exercice4_Afficher_Masquer_Texte/script.js
@@ -0,0 +1,5 @@
+// Sélectionner les éléments nécessaires
+
+// Ajouter un événement au bouton pour afficher ou masquer le texte
+
+// Vous pouvez utiliser la propriété `display` pour masquer ou afficher le texte
