1ddf7d19a08d268b5b4badcdc264fe31650f7517
AYMANE BENHIMA <aymanebenhima@gmail.com>
2024-08-26 19:26:33 +0100

    Create script.js

---
+++ Exercice5_Changer_Taille_Texte/script.js
@@ -0,0 +1,5 @@
+// Sélectionner les éléments nécessaires
+
+// Ajouter des événements aux boutons pour agrandir ou réduire la taille du texte
+
+// Vous pouvez utiliser la propriété `fontSize` pour modifier la taille du texte
