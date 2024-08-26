f00bff836baf20a5e697fa8d94dd29104bb96cfa
AYMANE BENHIMA <aymanebenhima@gmail.com>
2024-08-26 19:42:06 +0100

    Create script.js

---
+++ Exercice8_Filtrer_Liste/script.js
@@ -0,0 +1,5 @@
+// Sélectionner les éléments nécessaires
+
+// Ajouter un événement de saisie pour filtrer les éléments de la liste
+
+// Vous pouvez utiliser la méthode `includes` pour vérifier si le texte correspond
